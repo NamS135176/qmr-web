631312bf72debab0a7fd6f12d0d0b9a170ecaa28
namnt <namnt@vn-sis.com>
2021-10-06 09:30:04 +0700

    login api

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Container,
   Box,
@@ -6,173 +6,185 @@ import {
   TextField,
   InputBase,
   Button,
-} from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import logo from '../../assets/images/logo_en.png';
-import api from '../../api/api';
+} from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import logo from "../../assets/images/logo_en.png";
+import api from "../../api/api";
+import { useHistory } from "react-router-dom";
+
 const useStyle = makeStyles((theme: any) => ({
   container: {
-    backgroundColor: '#383e4b',
+    backgroundColor: "#383e4b",
     // height: '100vh',
     // display: 'flex',
     // flexDirection: 'row',
     // justifyContent: 'center',
-    boxSizing: 'border-box',
+    boxSizing: "border-box",
     // paddingBottom:100
-    paddingTop:50
-    ,
-    [theme.breakpoints.down('md')]: {
+    paddingTop: 50,
+    [theme.breakpoints.down("md")]: {
       // width: '80%',
       // marginTop: 30,
-      paddingTop:20
+      paddingTop: 20,
     },
   },
   loginModal: {
-    backgroundColor: 'white',
-    [theme.breakpoints.down('md')]: {
-      width: '80%',
+    backgroundColor: "white",
+    [theme.breakpoints.down("md")]: {
+      width: "80%",
       // marginTop: 30,
     },
-    [theme.breakpoints.up('md')]: {
-      width: '30%',
+    [theme.breakpoints.up("md")]: {
+      width: "30%",
     },
-    [theme.breakpoints.up('lg')]: {
-      width: '20%',
+    [theme.breakpoints.up("lg")]: {
+      width: "20%",
     },
     borderRadius: 5,
     // marginTop: 100,
     padding: 20,
     minWidth: 250,
-    marginLeft:`auto`,
-    marginRight:`auto`
+    marginLeft: `auto`,
+    marginRight: `auto`,
     // height:520,
-   
   },
   text: {
     fontSize: 15,
     margin: 0,
     padding: 0,
     lineHeightStep: 1,
-    lineHeight: '1.1',
+    lineHeight: "1.1",
   },
   textError: {
     fontSize: 13,
     margin: 0,
     padding: 0,
     lineHeightStep: 1,
-    lineHeight: '1.1',
-    color: 'red',
+    lineHeight: "1.1",
+    color: "red",
   },
   inputBase: {
     marginTop: 10,
-    background: '#e4e6eb',
-    width: '100%',
+    background: "#e4e6eb",
+    width: "100%",
     padding: theme.spacing(1, 1, 1, 2),
   },
   btn: {
     marginTop: 20,
     paddingBottom: 20,
     borderBottomWidth: 1,
-    borderColor: '#000',
+    borderColor: "#000",
   },
   link: {
     fontSize: 15,
     margin: 0,
     padding: 0,
     lineHeightStep: 1,
-    lineHeight: '1.1',
-    color: 'black',
-    '&:hover': {
-      color: '#428bca',
+    lineHeight: "1.1",
+    color: "black",
+    "&:hover": {
+      color: "#428bca",
     },
   },
 }));
 
 export default function LoginScreen() {
+  const history = useHistory();
   const classes = useStyle();
   const [showError, setShowError] = useState<boolean>(false);
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleChangeMail = (e:React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)
-  const handleChangePass = (e:React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)
+  const handleChangeMail = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setEmail(e.target.value);
+  const handleChangePass = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setPassword(e.target.value);
 
   const handleLogin = async () => {
-    const res = await api.post('/login',{
-      email:email,
-      password:password
-    })
-    console.log(res);
-    
-  }
+    setShowError(false);
+    try {
+      const res: any = await api.post("/login", {
+        email: email,
+        password: password,
+      });
+      console.log(res);
+      
+      history.push('/home')
+    } catch (error) {
+      console.log('sdfsdfsdf');
+      
+      setShowError(true)
+      // history.push('/home')
+    }
+  };
 
   return (
-    <Box sx={{
-      width:`100%`,
-      height:`100vh`,
-      backgroundColor: '#383e4b',
-      // paddingTop:10,
-      // paddingBottom:10
-    }}>
+    <Box
+      sx={{
+        width: `100%`,
+        height: `100vh`,
+        backgroundColor: "#383e4b",
+        // paddingTop:10,
+        // paddingBottom:10
+      }}
+    >
       <Box className={classes.container}>
-      <Box className={classes.loginModal}>
-        {showError ? (
-          <p className={classes.textError}>
-            Email or password is invalid. Please try again.
-          </p>
-        ) : (
-          <p className={classes.textError}></p>
-        )}
-        <img
-          src={logo}
-          width="100%"
-        />
-        <p className={classes.text}>Log in to Quick Money Recorder PC</p>
-        <p className={classes.text}>
-          (Please input email address and password which has been registered to
-          QMR Subscription)
-        </p>
-        <InputBase
-          type="email"
-          placeholder="type email"
-          className={classes.inputBase}
-          onChange={handleChangeMail}
-        ></InputBase>
-        <InputBase
-          type="password"
-          placeholder="password"
-          className={classes.inputBase}
-          onChange={handleChangePass}
-        ></InputBase>
-        <Box
-          sx={{
-            borderBottom: 1,
-            marginTop: 2,
-            paddingBottom: 3,
-            borderColor: '#eee',
-          }}
-        >
-          <Button
-            onClick={handleLogin}
-            variant="contained"
-            color="neutral"
-            fullWidth
-          >
-            Login
-          </Button>
-        </Box>
-        <Box sx={{ paddingTop: 2 }}>
+        <Box className={classes.loginModal}>
+          {showError ? (
+            <p className={classes.textError}>
+              Email or password is invalid. Please try again.
+            </p>
+          ) : (
+            <p className={classes.textError}></p>
+          )}
+          <img src={logo} width="100%" />
+          <p className={classes.text}>Log in to Quick Money Recorder PC</p>
           <p className={classes.text}>
-            Quick Money Recorder PC is Beta version. Some function may not work.
+            (Please input email address and password which has been registered
+            to QMR Subscription)
           </p>
-        </Box>
-        <Box sx={{ paddingTop: 1 }}>
-          <a className={classes.link} href="/forgot_password">
-            *If you forgot your password, re-issue new password here
-          </a>
+          <InputBase
+            type="email"
+            placeholder="type email"
+            className={classes.inputBase}
+            onChange={handleChangeMail}
+          ></InputBase>
+          <InputBase
+            type="password"
+            placeholder="password"
+            className={classes.inputBase}
+            onChange={handleChangePass}
+          ></InputBase>
+          <Box
+            sx={{
+              borderBottom: 1,
+              marginTop: 2,
+              paddingBottom: 3,
+              borderColor: "#eee",
+            }}
+          >
+            <Button
+              onClick={handleLogin}
+              variant="contained"
+              color="neutral"
+              fullWidth
+            >
+              Login
+            </Button>
+          </Box>
+          <Box sx={{ paddingTop: 2 }}>
+            <p className={classes.text}>
+              Quick Money Recorder PC is Beta version. Some function may not
+              work.
+            </p>
+          </Box>
+          <Box sx={{ paddingTop: 1 }}>
+            <a className={classes.link} href="/forgot_password">
+              *If you forgot your password, re-issue new password here
+            </a>
+          </Box>
         </Box>
       </Box>
-    </Box>
     </Box>
   );
 }
