6392b634d0c9b32e9b3a45868896f6e05a61fc03
NGUYENHOANGNAM <namnn@vn-sis.comnhnobnd@gmail.com>
2021-10-26 09:15:06 +0700

    2.44 2.45

--- src/utils/context.tsx
+++ src/utils/context.tsx
@@ -31,8 +31,10 @@ export function DateSelectProvider(props) {
   const [reloadPage, setReloadPage] = useState(false);
   const [listCategories, setListCategories] = useState([]);
   const getCate = async () => {
-    const res1: any = await getCategory();
-    setListCategories(res1.categories);
+    if (localStorage.getItem("access_token")) {
+      const res1: any = await getCategory();
+      setListCategories(res1.categories);
+    }
   };
   useEffect(() => {
     getCate();
